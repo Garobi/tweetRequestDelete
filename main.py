21fd457289b5cebcfd36c8081ad0bbbf4c2bd50f
Garobi <gabriel.rbittencourt15@gmail.com>
2024-08-06 00:46:46 -0300

    minor changes

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import json
 import authDataDelete
 import requests
 
-def deleteTweet(tweet):
+def deleteTweet(idTweet):
     requests.post(
         'https://x.com/i/api/graphql/VaenaVgh5q5ih7kvyVjgtg/DeleteTweet',
         cookies=authDataDelete.cookies,
         headers=authDataDelete.headers,
         json={
             'variables': {
-                'tweet_id': tweet['tweet']['tweet_id'],
+                'tweet_id': idTweet,
                 'dark_request': False,
             },
             'queryId': 'VaenaVgh5q5ih7kvyVjgtg',
@@ -22,7 +22,7 @@ def deleteTweet(tweet):
         headers=authDataDelete.headers,
         json={
             'variables': {
-                'tweet_id': tweet['tweet']['tweet_id'],
+                'tweet_id': idTweet,
                 'dark_request': False,
             },
             'queryId': 'iQtK4dl5hBmXewYZuEOKVw',
@@ -33,6 +33,7 @@ f = open("tweet-headers.js", "r")
 data_string = f.read()
 json_data = data_string.split('= ')[1]
 tweets = json.loads(json_data)
+lenTweet = len(tweets)
 
 date_format = "%a %b %d %H:%M:%S %z %Y"
 
@@ -40,7 +41,9 @@ last_month_naive = datetime.datetime.now() - datetime.timedelta(days=30)
 
 last_month = last_month_naive.replace(tzinfo=datetime.timezone.utc)
 
-for tweet in tweets:
+for index, tweet in enumerate(tweets):
     created_at = datetime.datetime.strptime(tweet['tweet']['created_at'], date_format)
+    idTweet = tweet['tweet']['tweet_id']
     if created_at < last_month:
-        deleteTweet(tweet)
+        deleteTweet(idTweet)
+        print(f'Tweet deleted id {idTweet}. {index} of {lenTweet}')
